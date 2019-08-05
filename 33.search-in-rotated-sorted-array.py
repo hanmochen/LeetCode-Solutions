45ea944816938663e5f489a66c4f99d2f139549d
Hanmo Chen <chm16@mails.tsinghua.edu.cn>
2019-08-05 11:44:25 +0800

    Updated to Problem 33
    Add another solution and notes to Problem 33
    Hanmo Chen 2019/08/05

--- 33.search-in-rotated-sorted-array.py
+++ 33.search-in-rotated-sorted-array.py
@@ -5,43 +5,23 @@
 #
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-            if len(nums)<=3:
-                try:
-                    return nums.index(target)
-                except ValueError:
-                    return -1
-            
-            def findMaxIndex(nums)-> int:
-                left,right = 0, len(nums)-1
-                while(left<right):
-                    mid = (left+right) // 2
-                    if nums[mid]>nums[mid+1]: return mid
-                    elif nums[mid]>nums[right]: left = mid
-                    else: right = mid
-                return -1 
-                        
-            def binary_search(nums,left,right,target)->int:
-                if(nums[left]>target or nums[right]<target): return -1
-                while(left<right):
-                    mid = (left+right) // 2
-                    if nums[mid]==target: return mid
-                    elif right-left == 1 : break
-                    elif nums[mid]<target: left = mid
-                    else: right = mid
-                return right if(nums[right]==target) else -1
-
-            maxIndex = findMaxIndex(nums)
-            if(maxIndex == -1):
-                return binary_search(nums,0,len(nums)-1,target)
-            
-            if(target<nums[0]):
-                return binary_search(nums,maxIndex+1,len(nums)-1,target)
-            else: 
-                return binary_search(nums,0,maxIndex,target)
-
-
+        if not nums: return -1
+        if nums[0] == target: return 0
         
+        low, high = 0, len(nums)
 
+        while(low < high):
+            mid = ( low + high ) // 2
+            if(nums[mid] == target): return mid
+            
+            if( (nums[mid]<nums[0]) == (target < nums[0]) ):
+                if(nums[mid]>target): high = mid
+                else: low = mid+1
+            
+            elif(nums[mid]>target): low = mid+1
+            else: high = mid
+        
+        return -1 
 
 
 
