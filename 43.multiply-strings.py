6680d2082e36614117a60070bad911f7f56d4303
Hanmo Chen <chm16@mails.tsinghua.edu.cn>
2019-08-12 10:18:29 +0800

    Updated to Problem 43
    Hanmo Chen 2019-08-12 10:18:14

---
+++ 43.multiply-strings.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=43 lang=python3
+#
+# [43] Multiply Strings
+#
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1)*int(num2))
+        
+
+
