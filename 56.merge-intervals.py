b3d680b896cca9a65de3c60e6bd8244deec7868b
Hanmo Chen <chm16@mails.tsinghua.edu.cn>
2019-08-19 13:51:16 +0800

    Update to Problem 56

--- 56.merge-intervals.py
+++ 56.merge-intervals.py
@@ -8,10 +8,11 @@ class Solution:
         mergedIntervals = sorted(intervals,key= lambda x: x[0])
         index = 0
         while(index< len(mergedIntervals)-1):
-            if mergedIntervals[index][1]>=mergedIntervals[index+1][0]:
+            if mergedIntervals[index][1]>= mergedIntervals[index+1][0]:
                 mergedInterval = [mergedIntervals[index][0],max(mergedIntervals[index][1],mergedIntervals[index+1][1])]
                 mergedIntervals[index+1]= mergedInterval
                 mergedIntervals.pop(index)
+
             else: index += 1
         return mergedIntervals
 
