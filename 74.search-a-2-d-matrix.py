2a1526cce45d7da8c403dbfe958d03a596ff73e0
Hanmo Chen <chm16@mails.tsinghua.edu.cn>
2019-09-01 16:33:42 +0800

    Updated to Problem 74

--- 74.search-a-2-d-matrix.py
+++ 74.search-a-2-d-matrix.py
@@ -4,12 +4,13 @@
 # [74] Search a 2D Matrix
 #
 class Solution:
-    def searchMatrix(self, matrix: [[int]], target: int) -> bool:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if not matrix: return False
         n = len(matrix)
         m = len(matrix[0])
         low,high = 0,n*m-1
-        while(low < high):
+        while(low <= high):
+            
             mid = (low+high)//2
             i,j = divmod(mid,m)
             if target==matrix[i][j]:
@@ -19,13 +20,4 @@ class Solution:
             else: high = mid-1
         return False
 
-
-                
-s = Solution()
-matrix = [
-  [1,   3,  5,  7],
-  [10, 11, 16, 20],
-  [23, 30, 34, 50]
-]
-target = 3
-print(s.searchMatrix(matrix,target))
+            
