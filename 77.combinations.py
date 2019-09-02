17236d29e380f69679d11f7331bf42045dbbb1e3
Hanmo Chen <chm16@mails.tsinghua.edu.cn>
2019-09-02 17:00:17 +0800

    Updated to Problem 77

--- 77.combinations.py
+++ 77.combinations.py
@@ -4,14 +4,10 @@
 # [77] Combinations
 #
 class Solution:
-    def combine(self, n: int, k: int) :
+    def combine(self, n: int, k: int) -> List[List[int]]:
         if n<k or k==0: return []
         if k == 1: return [[i] for i in range(1,n+1)]
         if n==k: return [list(range(1,n+1))]
         
         return [x+[n] for x in self.combine(n-1,k-1)]+self.combine(n-1,k)
-        
 
-s= Solution()
-
-print(s.combine(4,2))
