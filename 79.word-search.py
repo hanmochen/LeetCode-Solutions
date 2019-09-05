ce75a318f9b44dee842945acc243b6749a75bc8e
Hanmo Chen <chm16@mails.tsinghua.edu.cn>
2019-09-05 14:49:25 +0800

    Update to Problem 79

--- 79.word-search.py
+++ 79.word-search.py
@@ -4,35 +4,38 @@
 # [79] Word Search
 #
 
-import copy
 class Solution:
-    def exist(self, board: [[str]], word: str) -> bool:
+    def exist(self, board: List[List[str]], word: str) -> bool:
         if not board: return False
         h,w = len(board),len(board[0])
         isUsed = [[False]*w for _ in range(h)]
 
 
-        def DFS(x,y,word,isUsed):
+        def DFS(x,y,word):
             if not word: return True
             if x<0 or x>=len(board) or y<0 or y >= len(board[0]): return False
             if board[x][y] == word[0] and not isUsed[x][y]:
                 word_left = word[1:]
-                isUsed_left = copy.deepcopy(isUsed)
-                isUsed_left[x][y] = True
+                isUsed[x][y] = True
 
-                return DFS(x-1,y,word_left,isUsed_left) or DFS(x+1,y,word_left,isUsed_left) or DFS(x,y+1,word_left,isUsed_left) or DFS(x,y-1,word_left,isUsed_left)
+                if DFS(x-1,y,word_left):
+                    return True
+                if DFS(x+1,y,word_left):
+                    return True
+                if DFS(x,y-1,word_left):
+                    return True
+                if DFS(x,y+1,word_left):
+                    return True
+                
+                isUsed[x][y] = False
             
             return False
 
         
         for i in range(h):
             for j in range(w):
-                if DFS(i,j,word,isUsed):
-                    
+                if DFS(i,j,word):
                     return True
         return False
                 
-s= Solution()
-board =[["C","A","A"],["A","A","A"],["B","C","D"]]
-word = "AAB"
-print(s.exist(board,word))
+
