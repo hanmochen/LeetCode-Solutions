0d54e18cf19df520de7f2ad2d13b2160640e6530
Hanmo Chen <chm16@mails.tsinghua.edu.cn>
2019-09-06 13:58:43 +0800

    Updated to Problem 80

--- 80.remove-duplicates-from-sorted-array-ii.py
+++ 80.remove-duplicates-from-sorted-array-ii.py
@@ -4,28 +4,24 @@
 # [80] Remove Duplicates from Sorted Array II
 #
 class Solution:
-    def removeDuplicates(self, nums: [int]) -> int:
+    def removeDuplicates(self, nums: List[int]) -> int:
         if not nums: return 0
         temp = nums[0]
         pos = 1
         index = 1
 
         while(index < len(nums)):
-            count = nums[index] == temp 
             nums[pos] = nums[index]
             pos += 1
-            temp = nums[index]
-            index += 1
-            if count:                
+            if nums[index] == temp:  
+                index += 1  
                 while index<len(nums) and  nums[index] == temp:
                     index += 1
+            else:
+                temp = nums[index]
+                index += 1
         
         return pos
 
 
-                
-s= Solution()
-nums = [0]
 
-print(s.removeDuplicates(nums))
-print(nums)
